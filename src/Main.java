4255708b4ba4fae5f2ee34b50c3d1471b19023e8
elui1 <eric.lui2002@gmail.com>
2020-01-30 16:45:57 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean[] openLocker = new boolean[100];
+
+        for (int i = 1; i <= openLocker.length; i++) {
+            for (int j = i; j <= openLocker.length; j += i) {
+                if (openLocker[j - 1] == true) {
+                    openLocker[j - 1] = false;
+                }
+                else {
+                    openLocker[j - 1] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < openLocker.length; i++) {
+            if (openLocker[i] == true) {
+                System.out.println("Locker " + (i + 1) + " is open.");
+            }
+        }
+    }
+}
